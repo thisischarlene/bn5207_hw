7b540f95624720c89b637cd84cb93316082f00d1
thisischarlene <thisischarlene@icloud.com>
2025-02-02 13:54:29 +0800

    incl | base files;

---
+++ base_motion.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+/*_____________________________bn5207_homework_A0280349Y_____________________________*\
+<motion.py> { 
+    context         bn5207 homework on denoising images; 
+    purpose         for controlling imports in homework assignment; 
+    used in         [bn5207]; 
+    ref(s)          ;       
+    note            for main branch only; 
+} // "<motion.py>"
+/*_____________________________bn5207_homework_A0280349Y_____________________________*\
+"""
