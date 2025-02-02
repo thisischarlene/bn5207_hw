e0893c7cb37cf6726b0d7c013267c6bb2964e7b1
thisischarlene <thisischarlene@icloud.com>
2025-02-02 12:20:25 +0800

    incl | init file for all the baseline <.py>;

---
+++ __init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+/*_____________________________bn5207_homework_A0280349Y_____________________________*\
+<__init__.py> { 
+    context         bn5207 homework on denoising images; 
+    purpose         for controlling imports in homework assignment; 
+    used in         [bn5207_hw]; 
+    py ver          3.10; 
+    ref(s)          ;       
+    note            for main branch only; 
+} // "<__init__.py>"
+/*_____________________________bn5207_homework_A0280349Y_____________________________*\
+"""
+
+# baseline denoising method for each noise type; 
+from base_saltAndPepper import *
+from base_gaussian import *
+from base_motion import *
+from base_periodic import *
+
+
+# modified denoising method for each noise type; 
+
